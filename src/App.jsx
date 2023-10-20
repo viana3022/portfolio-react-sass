e2b1c13ffe86d2d4aeeb9161312b005ceb22b725
viana3022 <vianavinicius444@gmail.com>
2023-10-20 11:56:13 -0300

    Organização de arquivos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/components/app.sass';
+
+
+function App() {
+
+  return (
+    <div id="portifolio">
+      <h1>Vinícius Viana</h1>
+      
+    </div>
+  );
+}
+
+export default App;
